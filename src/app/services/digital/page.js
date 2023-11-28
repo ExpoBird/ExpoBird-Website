7a7dc8394ca6341684f35bc54e8dc26699d21986
Abdul Rehman <arboacha@gmail.com>
2023-11-28 15:39:17 +0500

    change

--- src/app/services/digital/page.js
+++ src/app/services/digital/page.js
@@ -1,13 +1,12 @@
 import DigitalHero from '@/app/components/DigitalHero';
 import TestingNav from '@/app/components/TestingNav';
 import Navbar from '@/app/components/Navbar';
-import Footer from '@/app/components/Footer';
 import OurService2 from '@/app/components/OurService2';
 import Driven2 from '@/app/components/driven2';
 import Faq2 from '@/app/components/Faq2';
-import CaseStudy2 from '@/app/components/CaseStudy2';
 import Footer2 from '@/app/components/Footer2';
 import FeatureWork from '@/app/components/FeatureWork';
+import CaseStudy from '@/app/components/CaseStudy';
 
 const App = () => {
   return (
@@ -19,7 +18,7 @@ const App = () => {
       </div>
          <OurService2 />
          <FeatureWork title={"We are a team of design and research experts with over ten years of experience in improving businesses and peoples’ lives through smart design"} />
-         <CaseStudy2 />
+         <CaseStudy />
           <Faq2 />
           <Driven2 />
           <Footer2 />
