2418420e3584c5463c1b1566fff53b327c3ca210
autodashboard <113629900+autodashboard@users.noreply.github.com>
2024-01-06 19:20:21 +0500

    changes

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,15 +1,48 @@
-import React, { Fragment } from 'react'
-import Navbar from '../components/Navbar'
-import TestingNav from '../components/TestingNav'
+"use client";
+
+import Script from "next/script";
+import Navbar from "../components/Navbar";
+import TestingNav from "../components/TestingNav";
+import DigitalHero from "../components/DigitalHero";
+import Contact from "../components/Contact";
 
 const page = () => {
   return (
-    <Fragment>
+    <>
+      <Script src="/scripts/script-3.js" strategy="lazyOnload"></Script>
+      <div className="bg-hero-image w-full overflow-hidden">
         <Navbar />
         <TestingNav />
-        <h1>Contact</h1>
-    </Fragment>
-  )
-}
+        <DigitalHero
+          title={
+            "ExpoBird is leading mobile app development company  Dubai, helping business transform their ideas into reality. We build mobile app \
+         development solutions to empower startups, SMBs, enterprises and brands."
+          }
+          text1={"We Are"}
+          text2={"Social Media Marketing"}
+          text3={"Company"}
+        />
+      </div>
+      <button
+        className="btn"
+        onClick={() => document.getElementById("my_modal_3").showModal()}
+      >
+        open modal
+      </button>
+      <dialog id="my_modal_3" className="modal modal-bottom sm:modal-middle w-[500px] p-3">
+        <div className="modal-box">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <Contact />
+        </div>
+      </dialog>
+      <Contact />
+    </>
+  );
+};
 
-export default page
+export default page;
