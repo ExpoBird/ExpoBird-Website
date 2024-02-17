28dd051f0db370c1b8409f56eea3134f961e7251
Abdul Rehman <arboacha@gmail.com>
2024-02-17 16:49:08 +0500

    changes

--- src/app/components/OurServices.js
+++ src/app/components/OurServices.js
@@ -9,15 +9,15 @@ const OurServices = () => {
         <div className="sm:px-10 md:px-30 px-48 sm:mt-10 md:mt-10">
           <div className="grid grid-cols-3 md:grid md:grid-cols-1 lg:grid lg:grid-cols-1 sm:block xl:grid xl:grid-cols-1 2xl:grid 2xl:grid-cols-1">
             <div className="sm:border-none md:items-center md:border-none lg:border-none xl:ml-[-110px] lg:ml-[-100px] font-montserrat xl:border-none border-r border-[#BDBDBD]">
-              <p className="text-black sm:text-[40px] leading-none text-[70px] mr-[10px] font-black md:text-[60px] xl:ml-[10px] lg:text-[50px] xl:text-[40px] lg:ml-[30px] mt-32 md:text-center sm:text-center sm:flex sm:items-center sm:m-auto md:flex md:items-center md:m-auto sm:justify-center md:justify-center">
+              <p className="text-black sm:text-[40px] leading-none text-[70px] mr-[10px] font-black md:text-[60px] xl:ml-[10px] lg:text-[50px] xl:text-[40px] lg:ml-[30px] mt-32 md:text-center sm:text-center sm:flex sm:items-center sm:m-auto md:flex md:items-center md:m-auto sm:justify-center md:justify-center lg:flex lg:items-center lg:m-auto lg:justify-center lg:mt-14 xl:flex xl:items-center xl:m-auto xl:justify-center xl:mt-14 xl:text-center">
                 Our Services
               </p>
-              <p className="sm:mt-10 md:mt-10 text-2xl md:text-center lg:ml-[30px] sm:text-center sm:text-[20px] text-black my-12 md:text-[20px] sm:flex sm:items-center sm:m-auto md:flex md:items-center md:m-auto">
+              <p className="sm:mt-10 md:mt-10 text-2xl md:text-center sm:text-center sm:text-[20px] text-black my-12 md:text-[20px] sm:flex sm:items-center sm:m-auto md:flex md:items-center md:m-auto lg:flex lg:items-center lg:m-auto lg:justify-center lg:mt-10 lg:text-center xl:flex xl:items-center xl:m-auto xl:justify-center xl:mt-10 xl:text-center">
                 Choose Expobird as your digital marketing agency and propel your
                 business to new heights with our award-winning digital marketing
                 services and proprietary technology platform.
               </p>
-              <button className="text-white sm:mb-1 sm:flex sm:items-center sm:m-auto md:flex md:items-center md:m-auto newColor text-[17px] rounded-[70px] lg:ml-[30px] py-4 px-6 font-semibold md:mt-10 sm:mt-0">
+              <button className="text-white sm:mb-1 sm:flex sm:items-center sm:m-auto md:flex md:items-center md:m-auto newColor text-[17px] rounded-[70px] py-4 px-6 font-semibold md:mt-10 sm:mt-0 lg:flex lg:items-center lg:m-auto lg:justify-center lg:mt-10 xl:flex xl:items-center xl:m-auto xl:justify-center xl:mt-10">
                 See All{" "}
                 <span>
                   <Image
