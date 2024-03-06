f5311c9eafaf1c3eff2b653d252d8e88ab66dc57
Abdul Rehman <arboacha@gmail.com>
2024-03-06 18:14:32 +0500

    changes

--- src/app/components/Service.jsx
+++ src/app/components/Service.jsx
@@ -11,7 +11,7 @@ const Service = () => {
               <div className="py-[2rem]">
                 <h5 className="text-[100px] font-black text-white">Services</h5>
               </div>
-              <div className="p-[2rem] sticky">
+              <div className="p-[2rem]">
                 <ul className="list-none">
                   <li className="text-[40px] text-[#7F7C9C] font-black sticky-text py-3">Digital Marketing</li>
                   <li className="text-[40px] text-[#7F7C9C] font-black sticky-text py-3">Social Media Marketing</li>
@@ -23,58 +23,58 @@ const Service = () => {
               </div>
             </div>
             <div className="col-span-6 border-l-2 border-[#7F7C9C]">
-              <div className="py-[3rem] h-screen">
+              <div className="pt-[3rem] h-screen">
                 <div>
                   <p className="text-[60px] text-white font-bold pl-5" data-inviewport >Digital Marketing</p>
                   <p className="text-[20px] text-white font-medium max-w-[50%] pl-5 py-3">We imagine and build experiences and businesses that win hearts and realize the future. Explore how we work.</p>
-                  <Image src="/assets/digital-branding.webp" height={100} width={600} className="py-3" alt="" />
+                  <Image src="/assets/digital-branding.webp" height={200} width={700} className="py-3" alt="" />
                   <button className="pt-8"><Image src="/assets/takeALook.svg" height={78} width={279} className="pl-5" alt="" /></button>
                 </div>
               </div>
-              <div className="py-[3rem] h-screen">
+              <div className="pt-[3rem] h-screen">
                 <div>
                   <p className="text-[60px] text-white font-bold pl-5" data-inviewport >Social Media Marketing</p>
                   <p className="text-[20px] text-white font-medium max-w-[50%] pl-5 py-3">We imagine and build experiences and businesses that win hearts and realize the future. Explore how we work.</p>
-                  <Image src="/assets/digital-marketing.webp" height={100} width={600} className="py-3" alt="" />
+                  <Image src="/assets/digital-marketing.webp" height={200} width={700} className="py-3" alt="" />
                   <button className="pt-8"><Image src="/assets/takeALook.svg" height={78} width={279} className="pl-5" alt="" /></button>
                 </div>
               </div>
-              <div className="py-[3rem] h-screen">
+              <div className="pt-[3rem] h-screen">
                 <div>
                   <p className="text-[60px] text-white font-bold pl-5" data-inviewport >Mobile Development</p>
                   <p className="text-[20px] text-white font-medium max-w-[50%] pl-5 py-3">We imagine and build experiences and businesses that win hearts and realize the future. Explore how we work.</p>
-                  <Image src="/assets/ecommerce-development.webp" height={100} width={600} className="py-3" alt="" />
+                  <Image src="/assets/ecommerce-development.webp" height={200} width={700} className="py-3" alt="" />
                   <button className="pt-8"><Image src="/assets/takeALook.svg" height={78} width={279} className="pl-5" alt="" /></button>
                 </div>
               </div>
-              <div className="py-[3rem] h-screen">
+              <div className="pt-[3rem] h-screen">
                 <div>
                   <p className="text-[60px] text-white font-bold pl-5" data-inviewport >Web Development</p>
                   <p className="text-[20px] text-white font-medium max-w-[50%] pl-5 py-3">We imagine and build experiences and businesses that win hearts and realize the future. Explore how we work.</p>
-                  <Image src="/assets/web-development.webp" height={100} width={600} className="py-3" alt="" />
+                  <Image src="/assets/web-development.webp" height={200} width={700} className="py-3" alt="" />
                   <button className="pt-8"><Image src="/assets/takeALook.svg" height={78} width={279} className="pl-5" alt="" /></button>
                 </div>
               </div>
-              <div className="py-[3rem] h-screen">
+              <div className="pt-[3rem] h-screen">
                 <div>
                   <p className="text-[60px] text-white font-bold pl-5" data-inviewport >Ui/Ux Design</p>
                   <p className="text-[20px] text-white font-medium max-w-[50%] pl-5 py-3">We imagine and build experiences and businesses that win hearts and realize the future. Explore how we work.</p>
-                  <Image src="/assets/ui-ux-design.webp" height={100} width={600} className="py-3" alt="" />
+                  <Image src="/assets/ui-ux-design.webp" height={200} width={700} className="py-3" alt="" />
                   <button className="pt-8"><Image src="/assets/takeALook.svg" height={78} width={279} className="pl-5" alt="" /></button>
                 </div>
               </div>
-              <div className="py-[3rem] h-screen">
+              <div className="pt-[3rem] h-screen">
                 <div>
                   <p className="text-[60px] text-white font-bold pl-5" data-inviewport >Graphic Designing</p>
                   <p className="text-[20px] text-white font-medium max-w-[50%] pl-5 py-3">We imagine and build experiences and businesses that win hearts and realize the future. Explore how we work.</p>
-                  <Image src="/assets/ecommerce-development.webp" height={100} width={600} className="py-3" alt="" />
+                  <Image src="/assets/ecommerce-development.webp" height={200} width={700} className="py-3" alt="" />
                   <button className="pt-8"><Image src="/assets/takeALook.svg" height={78} width={279} className="pl-5" alt="" /></button>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        </div>
+      </div>
     </Fragment>
   )
 }
